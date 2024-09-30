1ae1797793f601e2564b125790ac3fe6e42a7273
parinithaopsera456 <gparinitha@opsera.io>
2024-09-30 19:54:05 +0530

    Update main.tf

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,29 +1,42 @@
 # main.tf
 
-variable "acc-key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "secret" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket to create"
-  type        = string
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.acc-key  # Use the access key variable
+  secret_key = var.secret   # Use the secret key variable
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  # Removed the acl setting
+  bucket = var.bucket_name  # Use the bucket name variable
 }
 
 resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   bucket = aws_s3_bucket.my_bucket.id
 
   versioning_configuration {
-    status = "Enabled"  # Set the status here
+    status = "Enabled"  # Set the status for versioning
   }
+}
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.my_bucket.arn}",
+          "${aws_s3_bucket.my_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
