9938d9ba8727103b3b6a1c53915209e99d9e3169
parinithaopsera456 <gparinitha@opsera.io>
2024-09-30 19:39:52 +0530

    Update provider.tf

--- modules/app/provider.tf
+++ modules/app/provider.tf
@@ -1,9 +1,8 @@
 # provider.tf
 
 provider "aws" {
-  region = "us-east-1"
-
-  # Optionally specify access and secret keys
-   access_key = "acc-key"
-   secret_key = "secret"
+  region     = "us-east-1"
+  access_key = var.acc-key
+  secret_key = var.secret
 }
+
