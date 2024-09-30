c47ac9684113d5997c660816a4615042d2883999
parinithaopsera456 <gparinitha@opsera.io>
2024-09-30 21:39:18 +0530

    Update main.tf

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,22 +1,18 @@
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
+# main.tf
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   bucket = aws_s3_bucket.my_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.my_bucket.arn}",
-          "${aws_s3_bucket.my_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
