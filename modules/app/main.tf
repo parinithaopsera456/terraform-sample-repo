f390c552344e2f1e0b9cf3bed7b0c3837005be4f
parinithaopsera456 <gparinitha@opsera.io>
2024-09-30 19:50:47 +0530

    Update main.tf

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -10,20 +10,19 @@ variable "secret" {
   type        = string
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
+variable "bucket_name" {
+  description = "The name of the S3 bucket to create"
+  type        = string
 }
 
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
-  bucket = aws_s3_bucket.my_bucket.id
-  acl    = "private"  # Setting the ACL here
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  # Removed the acl setting
 }
 
 resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   bucket = aws_s3_bucket.my_bucket.id
 
-  # Corrected versioning configuration
   versioning_configuration {
     status = "Enabled"  # Set the status here
   }
