17df6d5296e920984225257924aa28ad1a676002
parinithaopsera456 <gparinitha@opsera.io>
2024-09-30 17:34:54 +0530

    Update variables.tf

--- modules/app/variables.tf
+++ modules/app/variables.tf
@@ -1,51 +1,7 @@
-variable "vpc_id" {}
+# variables.tf
 
-variable "vpc_subnets" {
-  type = "list"
-}
-
-variable "desired_capacity" {
-  default = 1
-}
-
-variable "max_size" {
-  default = 1
-}
-
-variable "bastion_sg_id" {
-  default = ""
-}
-
-variable "key_name" {}
-
-variable "image_id" {
-  default = ""
-}
-
-variable "instance_type" {
-  default = ""
-}
-
-variable "root_volume_size" {
-  default = "8"
-}
-
-variable "zone_id" {}
-
-variable "env" {}
-
-variable "vpc_security_groups" {
-  default = []
-}
-
-variable "vpc_cidr_blocks" {
-  default = []
-}
-
-variable "app_security_groups" {
-  default = []
-}
-
-variable "alb_public_subnets" {
-  default = []
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "samples3bucketpari"
 }
